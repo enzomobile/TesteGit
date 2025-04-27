7b8b73c54b0ce3b4f9a63af48e32c01a24eed6e0
Enzo Móbile de Oliveira <prenzo0001@gmail.com>
2025-04-26 21:59:48 -0300

    Adicionei pastas e arquivos e mudei a posição do readme, apenas para teste.

---
+++ src/front-end/pages/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/style.css">
+    <title>Página de teste</title>
+</head>
+<body>
+    <h1>Apenas teste</h1>
+    <p>Esse arquivo, junto dos outros, foram feitos pelo terminal, só estou usando alguns comandos pra aprende-los.</p>
+</body>
+</html>
