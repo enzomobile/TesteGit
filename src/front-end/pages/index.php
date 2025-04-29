e5b8c67db20a20bcf2ed1bf0f1668d79c9d94905
Enzo Móbile de Oliveira <prenzo0001@gmail.com>
2025-04-29 16:32:10 -0300

    Fiz uma branch para adicionar um função de soma de 2 valores, apenas para testar a criação de branch e merge.

--- src/front-end/pages/index.php
+++ src/front-end/pages/index.php
@@ -1,3 +1,22 @@
+<?php
+
+    $valor1 = 0;
+    $valor2 = 0;
+    $result = 0;
+
+    function soma($a, $b) {
+        $resultado = $a + $b;
+        return $resultado;
+    }
+
+    if ($_POST['REQUEST_METHOD'] == 'POST') {
+        $valor1 = $_POST['valor1'];
+        $valor2 = $_POST['valor2'];
+        $result = soma($valor1, $valor2);
+        return $result;
+    }
+    
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,5 +28,15 @@
 <body>
     <h1>Apenas teste</h1>
     <p>Esse arquivo, junto dos outros, foram feitos pelo terminal, só estou usando alguns comandos pra aprende-los.</p>
+    <br><br>
+    <form action="index.php" method="POST">
+        <input type="number" name="valor1" id="valor1">
+        <br>
+        <input type="number" name="valor2" id="valor2">
+        <br>
+        <input type="number" name="resultado" id="resultado" value="<?=$result;?>" readonly>
+        <br>
+        <button type="submit">Ver Soma</button>
+    </form>
 </body>
 </html>
